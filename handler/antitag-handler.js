4a58d0455705e9cc9beae871d03848a2d00570d4
dawens8 <mydawens8@gmail.com>
2025-07-15 00:50:29 -0400

    Delete handler/antitag-handler.js

--- handler/antitag-handler.js
+++
@@ -1,8 +0,0 @@
-const { antiTagHandler } = require('./plugins/antitag');
-
-conn.ev.on('messages.upsert', async (msg) => {
-  const m = msg.messages[0];
-  if (!m.key.fromMe && m.key.remoteJid?.endsWith('@g.us')) {
-    await antiTagHandler(m, conn);
-  }
-});
